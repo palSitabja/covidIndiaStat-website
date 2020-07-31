7795da098b9fb0ce69cced0716273ed30fb8383c
Sitabja <sitabjapal03@gmail.com>
2020-07-31 23:41:05 +0530

    1. UI modification, 2. chart library changed to apexchart from chart-js

--- public/js/app.js
+++ public/js/app.js
@@ -1,257 +1,286 @@
 const url='/data'
 fetch(url).then((response)=>{
     response.json().then((data)=>{
-        //console.log(data[0])
-        const chart1=document.getElementById('line1')
-        chart1.height=300;
-        let lineChart1=new Chart(chart1,{
-            type:'line',
-            data:{
-                labels:data[0][6],
-                datasets:[{
-                    label:'Daily Confirmed',
-                    backgroundColor:'#FDF3BB',
-                    borderColor:'#BA5200',
-                    data:data[0][0],
-                    fill:true
-                },
-                {
-                    label:'Total Confirmed',
-                    backgroundColor:'#fc9272',
-                    borderColor:'#fc9272',
-                    data:data[0][3],
-                    fill:false
-                }]
+        var options_daily = {
+            chart: {
+              height: 'auto',
+              type: "line",
+              redrawOnParentResize: true,
+              toolbar: {
+                show: false
+              }
             },
-            options:{
-                scales:{
-                    xAxes:[{
-                        gridLines:{
-                            drawOnChartArea:false
-                        }
-                    }],
-                    yAxes:[{
-                        gridLines:{
-                            drawOnChartArea:false
-                        },
-                        // ticks: {
-                        //     stepSize: 10000
-                        //  },
-                    }],
-                },
-                responsive: true,
-                maintainAspectRatio: false,
-                title: {
-					display: true,
-					text: 'Confirmed Cases'
-                },
-                tooltips: {
-					mode: 'index',
-                    intersect: false,
-                    backgroundColor	:'rgba(255, 255, 255, 0.1)',
-                    titleFontColor:'#313440',
-                    bodyFontColor:'#313440',
-                    bodyFontStyle:'bold',
-                    borderColor: '#BA5200',
-                    borderWidth: 2,
-				},
-				hover: {
-					mode: 'nearest',
-					intersect: true
-                },
-            }    
-        })
-        const chart2=document.getElementById('line2')
-        //chart2.height=400;
-        let lineChart2=new Chart(chart2,{
-            type:'line',
-            data:{
-                labels:data[0][6],
-                datasets:[{
-                    label:'Daily Deceased',
-                    backgroundColor:'#FDBBCA',
-                    borderColor:'#E81E51',
-                    data:data[0][1],
-                    fill:true
-                },
-                {
-                    label:'Total Deceased',
-                    backgroundColor:'#FB9EA3',
-                    borderColor:'#FB9EA3',
-                    data:data[0][4],
-                    fill:false
-                }]
+            colors:['#3F51B5','#61D800',],
+            grid: {
+                show: false,
             },
-            options:{
-                scales:{
-                    xAxes:[{
-                        gridLines:{
-                            drawOnChartArea:false
-                        }
-                    }],
-                    yAxes:[{
-                        gridLines:{
-                            drawOnChartArea:false
-                        },
-                        ticks: {
-                            //stepSize: 500
-                         },
-                    }],
-                },
-                responsive: true,
-                maintainAspectRatio: false,
-                title: {
-					display: true,
-					text: 'Deceased'
-                },
-                tooltips: {
-					mode: 'index',
-                    intersect: false,
-                    backgroundColor	:'rgba(255, 255, 255, 0.1)',
-                    titleFontColor:'#313440',
-                    bodyFontColor:'#313440',
-                    bodyFontStyle:'bold',
-                    borderColor: '#EC0D45',
-                    borderWidth: 2,
-				},
-				hover: {
-					mode: 'nearest',
-					intersect: true
-                },
-            }    
-        })
-
-
-        const chart3=document.getElementById('line3')
-        //chart3.height=400;
-        let lineChart3=new Chart(chart3,{
-            type:'line',
-            data:{
-                labels:data[0][6],
-                datasets:[{
-                    label:'Daily Recovered',
-                    backgroundColor:'#BBFDC7',
-                    borderColor:'#31a354',
-                    data:data[0][2],
-                    fill:true
-                },
-                {
-                    label:'Total Recovered',
-                    backgroundColor:'#addd8e',
-                    borderColor:'#addd8e',
-                    data:data[0][5],
-                    fill:false
-                }]
+            dataLabels: {
+              enabled: false
             },
-            options:{
-                scales:{
-                    xAxes:[{
-                        gridLines:{
-                            drawOnChartArea:false
-                        }
-                    }],
-                    yAxes:[{
-                        // ticks: {
-                        //     stepSize: 1000
-                        //  },
-                        gridLines:{
-                            drawOnChartArea:false
-                        }
-                    }],
-                },
-                responsive: true,
-                maintainAspectRatio: false,
-                title: {
-					display: true,
-					text: 'Recovery'
-                },
-                tooltips: {
-					mode: 'index',
-                    intersect: false,
-                    backgroundColor	:'rgba(255, 255, 255, 0.1)',
-                    titleFontColor:'#313440',
-                    bodyFontColor:'#313440',
-                    bodyFontStyle:'bold',
-                    borderColor: '#36BB60',
-                    borderWidth: 2,
-				},
-				hover: {
-					mode: 'nearest',
-					intersect: true
-                },
-            }    
-        })
-
-
-        const bar1=document.getElementById('bar1')
-        //chart3.height=800;
-        let barchart1=new Chart(bar1,{
-            type:'bar',
-            data:{
-                labels:data[1][3],
-                datasets:[{
-                    label:'Active',
-                    backgroundColor:'#2b8cbe',
-                    borderColor:'#2b8cbe',
-                    data:data[1][0],
-                    fill:true
-                },
-                {
-                    label:'confirmed',
-                    backgroundColor:'#a6bddb',
-                    borderColor:'#a6bddb',
-                    data:data[1][1],
-                    fill:true
-                },
-                {
-                    label:'deaths',
-                    backgroundColor:'#E81E44',
-                    borderColor:'#E81E44',
-                    data:data[1][2],
-                    fill:true
-                }]
+            stroke: {
+              width: 4,
+                  // colors:['#008FFB','#ff0000','#21891d'],
+              },
+            series: [
+              {
+                type: "line",
+                name: "Daily Confirmed",
+                data: data[0][0]
+              },
+              {
+                type: "area",
+                name: "Daily Recovery",
+                data: data[0][2],
+              },
+            ],
+            fill: {
+              type:'solid',
+              opacity: [1, 0.2],
+              // colors:['#008FFB','#ff0000','#21891d'],
+              // type: "gradient",
+              // gradient: {
+              //   shadeIntensity: 1,
+              //   opacityFrom: 0.7,
+              //   opacityTo: 0.9,
+              //   stops: [0, 90, 100]
+              // }
             },
-            options:{
-                responsive: true,
-                maintainAspectRatio: false,
-                title: {
-					display: true,
-					text: 'State Wise'
+            xaxis: {
+                labels: {
+                    show: false,
                 },
-                tooltips: {
-					mode: 'index',
-                    intersect: false,
-                    backgroundColor	:'rgba(255, 255, 255, 0.1)',
-                    titleFontColor:'#313440',
-                    bodyFontColor:'#313440',
-                    bodyFontStyle:'bold',
-                    borderColor: '#2B8CBE',
-                    borderWidth: 2,
-				},
-				hover: {
-					mode: 'nearest',
-					intersect: true
+              categories:data[0][6],
+            },
+            yaxis: {//to make yaxis visible comment this part 'yaxis{}'
+                labels: {
+                    show: false,
                 },
-                scales: {
-                    xAxes: [{
-                        gridLines:{
-                            drawOnChartArea:false
-                        },
-                        stacked: true,
-                    }],
-                    yAxes: [{
-                        gridLines:{
-                            drawOnChartArea:false
-                        },
-                        stacked: true
-                    }]
-                }
-            }    
-        })
-        //console.log(parseInt(data[2][1]))
-        //console.log(parseInt(data[1][1][0]))
-        document.getElementById("conf").innerHTML=parseInt(data[1][1][0])//+parseInt(data[2][0])+
-        document.getElementById("recover").innerHTML=parseInt(data[1][4][0])//+parseInt(data[2][2])
-        document.getElementById("dec").innerHTML=parseInt(data[1][2][0])//+parseInt(data[2][1])
+            },
+            legend: {
+              position: 'top',
+              offsetY: 40,
+              fontSize: '14px',
+              fontfamily: 'Poppins'
+            },
+        };
+        var options_total = {
+            chart: {
+              height: 'auto',
+              type: "line",
+              redrawOnParentResize: true,
+              toolbar: {
+                show: false
+              }
+            },
+            colors:['#29B6F6','#009688',],
+            grid: {
+                show: false,
+            },
+            dataLabels: {
+              enabled: false
+            },
+            stroke: {
+                width: 4,
+                    // colors:['#008FFB','#ff0000','#21891d'],
+            },
+            series: [
+              {
+                name: "Total Confirm",
+                data:data[0][3],
+                type: "line",
+              },
+              {
+                name: "Total Recovery",
+                data: data[0][5],
+                type: "area",
+              },
+            //   {
+            //     name: "Total Deceased",
+            //     data: data[0][4],
+            //   }
+            ],
+            fill: {
+              type:'solid',
+              opacity: [1, 0.2],
+              // colors:['#008FFB','#ff0000','#21891d'],
+              // type: "gradient",
+              // gradient: {
+              //   shadeIntensity: 1,
+              //   opacityFrom: 0.7,
+              //   opacityTo: 0.9,
+              //   stops: [0, 90, 100]
+              // }
+            },
+            xaxis: {
+                labels: {
+                    show: false,
+                },
+              categories:data[0][6],
+            },
+            yaxis: {//to make yaxis visible comment this part 'yaxis{}'
+                labels: {
+                    show: false,
+                },
+            },
+            legend: {
+              position: 'top',
+              offsetY: 40,
+              fontSize: '14px',
+              fontfamily: 'Poppins'
+            },
+          };
+          var options_death = {
+            chart: {
+              height: 'auto',
+              // width:'100%',
+              type: "area",
+              redrawOnParentResize: true,
+              toolbar: {
+                show: false
+              }
+            },
+            colors:['#F06292','#FF8A80',],
+            grid: {
+                show: false,
+            },
+            dataLabels: {
+              enabled: false
+            },
+            stroke: {
+              width: 4,
+                  // colors:['#008FFB','#ff0000','#21891d'],
+              },
+            series: [
+              {
+                name: "Total Deceased",
+                data: data[0][4]
+              },
+            //   {
+            //     name: "Daily Recovery",
+            //     data: data[0][2],
+            //   },
+              {
+                // type:'line',
+                name: "Daily Deceased",
+                data: data[0][1],
+              }
+            ],
+            fill: {
+              // colors:['#008FFB','#ff0000','#21891d'],
+              type: "gradient",
+              gradient: {
+                shadeIntensity: 1,
+                opacityFrom: 0.7,
+                opacityTo: 0.9,
+                stops: [0, 90, 100]
+              }
+            },
+            xaxis: {
+                labels: {
+                    show: false,
+                },
+              categories:data[0][6],
+            },
+            yaxis: {//to make yaxis visible comment this part 'yaxis{}'
+                labels: {
+                    show: false,
+                },
+            },
+            legend: {
+              position: 'top',
+              offsetY: 40,
+              fontSize: '14px',
+              fontfamily: 'Poppins'
+            },
+        };
+          var options_stateWise = {
+            series: [{
+            name: 'Active',
+            data:data[1][0],
+          }, {
+            name: 'Confirmed',
+            data: data[1][1],
+          }, {
+            name: 'Deaths',
+            data:data[1][2],
+          },],
+            chart: {
+            type: 'bar',
+            height: 350,
+            stacked: true,
+
+            toolbar: {
+                show: false
+              }
+        
+          },
+          dataLabels: {
+            enabled: false
+          },
+          colors:['#6200EE','#E0D2FC','#F06292',],
+          responsive: [{
+            breakpoint: 480,
+            options: {
+              legend: {
+                position: 'bottom',
+                offsetX: -10,
+                offsetY: 0
+              }
+            }
+          }],
+          plotOptions: {
+            bar: {
+              horizontal: false,
+            },
+          },
+          xaxis: {
+            
+            categories: data[1][3],
+          },
+          yaxis:{
+            labels: {
+              show: false,
+            },
+          },
+          legend: {
+            position: 'right',
+            offsetY: 40,
+            fontSize: '14px',
+            fontfamily: 'Poppins'
+          },
+          fill: {
+            opacity: 0.8
+          }
+          };
+        var chart_daily = new ApexCharts(document.querySelector("#chart1"), options_daily);
+        var chart_total = new ApexCharts(document.querySelector("#chart2"), options_total);
+        var chart_stateWise = new ApexCharts(document.querySelector("#chart_ststeWise"), options_stateWise);
+        var chart_death=new ApexCharts(document.querySelector("#chart_death"),options_death);
+        chart_daily.render();
+        chart_total.render();
+        chart_stateWise.render();
+        chart_death.render();
     })
 })
+// function toogleModal(event) {
+//   var modal = document.getElementById("myModal");
+//   var span = document.getElementsByClassName("close")[0];
+//   modal.style.display = "block";
+//   // event.preventDefault();
+// }
+var modal = document.getElementById("myModal");
+document.getElementById("about_btn").addEventListener("click", function(event){
+  
+  
+  modal.style.display = "block";
+  event.preventDefault()
+});
+var span = document.getElementsByClassName("close")[0];
+span.onclick = function() {
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
